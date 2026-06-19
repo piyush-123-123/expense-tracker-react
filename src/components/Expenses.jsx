1e09c586b7398e6f1c0604b21ee5b70ca9a85534
Your Name <your-email@example.com>
2026-06-19 21:13:16 +0530

    Expense Tracker React Project(1st project using react components

---
+++ src/components/Expenses.jsx
@@ -0,0 +1,20 @@
+import "./Expenses.css";
+import Card from "./Card";
+import ExpenseItem from "./ExpenseItem"
+function Expenses(props) {
+  return (
+    <Card className="expenses">
+      {props.expenses.map((expense) => {
+        return (
+          <ExpenseItem
+            id={expense.id}
+            title={expense.title}
+            date={expense.date}
+            price={expense.price}
+          />
+        )
+      })}
+    </Card>
+  );
+}
+export default Expenses;
