05bf1ad859f70ffefeea5a561ea59fdc1667a288
Your Name <your-email@example.com>
2026-06-21 13:15:56 +0530

    Implemented useState in Expense Item

--- src/components/ExpenseItem.jsx
+++ src/components/ExpenseItem.jsx
@@ -2,13 +2,20 @@
 import "./ExpenseItem.css";
 import ExpenseDate from "./ExpenseDate";
 import Card from "./Card";
+import { useState } from "react";
 function ExpenseItem(props) {
+const[title,setTitle]=useState(props.title);
+ function  buttonClickHandler() {
+  setTitle("New Title");
+  }
+
   return (
     <Card className="expense-item">
       <ExpenseDate date={props.date} />
       <div className="expense-item__descriptio">
-        <h2>{props.title}</h2>
+        <h2>{title}</h2>
         <div className="expense-item__price">${props.price}</div>
+        <button onClick={buttonClickHandler}>Change Title</button>
       </div>
     </Card>
   )
